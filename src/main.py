a12b1b6194f29865b8754f7a98a0be577fc0bb9a
Md Sazzad Islam <sazzad.islam@dsinnovators.com>
2017-07-10 18:11:45 +0600

    data validation added with marshmello lib π

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
 from flask import Flask, request, jsonify
+from marshmallow import Schema, fields, validate
 
 app = Flask(__name__)
+
+
+class UserSchema(Schema):
+    name = fields.String(required=True)
+    age = fields.Integer(
+        required=True,
+        error_messages={'required': 'Age is required.'}
+    )
+    city = fields.String(
+        required=True,
+        error_messages={'required': {'message': 'City required', 'code': 400}}
+    )
+    email = fields.Str(required=True,
+                       validate=validate.Email(error='Not a valid email address'))
+
+
+userSchema = UserSchema()
 
 
 @app.route('/')
@@ -27,6 +45,13 @@ def postTest():
 def newPost():
     language = {"name": request.json['name']}
     return jsonify({"List": language})
+
+
+@app.route("/users", methods=['POST'])
+def users():
+    user_json = request.json
+    result = userSchema.load(user_json)
+    return jsonify({"Result": result})
 
 
 if __name__ == "__main__":
