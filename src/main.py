71a98c7c082fdfb6a3899a60632c3ee3e843915b
Md Sazzad Islam <sazzad.islam@dsinnovators.com>
2017-07-10 14:31:00 +0600

    diff url testedˆ

--- src/main.py
+++ src/main.py
@@ -8,5 +8,15 @@ def index():
     return 'this is the homepage'
 
 
+@app.route("/profile/<username>")
+def profile(username):
+    return "hey %s" % username
+
+
+@app.route("/num/<int:postId>")
+def getNumber(postId):
+    return "posted Number %s" % postId
+
+
 if __name__ == "__main__":
     app.run(debug=True)
