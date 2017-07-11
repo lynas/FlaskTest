353321f2544c53f93c9ea35863a69d8a1b02079b
Md Sazzad Islam <sazzad.islam@dsinnovators.com>
2017-07-11 20:20:36 +0600

    clean up

--- src/main.py
+++ src/main.py
@@ -11,35 +11,10 @@ app.config['MONGO_DBNAME'] = 'flsk'
 mongo = PyMongo(app, config_prefix='MONGO')
 
 
-# @app.route('/db')
-# def db():
-#    return jsonify(getAppUserList())
-
-
 @app.route('/')
 def index():
     return 'this is the homepage'
 
-
-@app.route("/profile/<username>")
-def profile(username):
-    return "hey %s" % username
-
-
-@app.route("/num/<int:postId>")
-def getNumber(postId):
-    return "posted Number %s" % postId
-
-
-@app.route("/postTest", methods=['POST'])
-def postTest():
-    return "Post method Test"
-
-
-@app.route("/newPost", methods=['POST'])
-def newPost():
-    language = {"name": request.json['name']}
-    return jsonify({"List": language})
 
 
 @app.route("/users", methods=['POST'])
