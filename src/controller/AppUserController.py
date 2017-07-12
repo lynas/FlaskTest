f936f2da2ee239d5747eddf07eaa895a5f155da6
Md Sazzad Islam <sazzad.islam@dsinnovators.com>
2017-07-12 12:55:08 +0600

    get by name doneπ

--- src/controller/AppUserController.py
+++ src/controller/AppUserController.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from service.AppUserService import getAppUserList
+from service.AppUserService import getOneAppUserByName
 
 app_user_api = Blueprint("app_user_api", __name__)
 
@@ -9,9 +10,9 @@ def appUserList():
     return jsonify(getAppUserList())
 
 
-@app_user_api.route("/<appUserId>")
-def getAppUser(appUserId):
-    return "AppUser with id %s" % appUserId
+@app_user_api.route("/<firstName>")
+def getAppUser(firstName):
+    return jsonify(getOneAppUserByName(firstName))
 
 
 @app_user_api.route("/", methods=["POST"])
--- src/service/AppUserService.py
+++ src/service/AppUserService.py
@@ -6,3 +6,9 @@ def getAppUserList():
     for q in mongoDB.db.AppUser.find():
         output.append({'firstName': q['firstName'], 'lastName': q['lastName']})
     return output
+
+
+def getOneAppUserByName(name):
+    q = mongoDB.db.AppUser.find_one_or_404({'firstName': name})
+    output = {'firstName': q['firstName'], 'lastName': q['lastName']}
+    return output
