efe4a89552b7341afa2a5139b0a419a2944617de
Md Sazzad Islam <sazzad.islam@dsinnovators.com>
2017-07-12 15:16:03 +0600

    accessing id

--- src/schema/AppUser.py
+++ src/schema/AppUser.py
@@ -2,9 +2,10 @@ from marshmallow import Schema, fields, validate
 
 
 class AppUser(Schema):
+    _id = fields.String(required=False)
     firstName = fields.String(required=True)
     lastName = fields.String(required=True)
     age = fields.Integer(
-        required=True,
+        required=False,
         error_messages={'required': 'Age is required.'}
     )
--- src/service/AppUserService.py
+++ src/service/AppUserService.py
@@ -1,21 +1,25 @@
 from config.DBConfig import mongoDB
+from schema.AppUser import AppUser
 
 
 def getAppUserList():
     output = []
     for q in mongoDB.db.AppUser.find():
-        output.append({'firstName': q['firstName'], 'lastName': q['lastName']})
+        data, errors = AppUser().load(q)
+        data['_id'] = str(q['_id'])
+        output.append(data)
     return output
 
 
 def getOneAppUserByName(name):
     q = mongoDB.db.AppUser.find_one_or_404({'firstName': name})
-    output = {'firstName': q['firstName'], 'lastName': q['lastName']}
-    return output
+    data, errors = AppUser().load(q)
+    data['_id'] = str(q['_id'])
+    return data
 
 
 def createNewAppUser(appUser):
     app_user_id = mongoDB.db.AppUser.insert(appUser)
     q = mongoDB.db.AppUser.find_one_or_404({'_id': app_user_id})
-    output = {'firstName': q['firstName'], 'lastName': q['lastName']}
-    return output
+    data, errors = AppUser().load(q)
+    return data
