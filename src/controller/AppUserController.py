b1b82759dddbc18b829868235b7f086140709723
Md Sazzad Islam <sazzad.islam@dsinnovators.com>
2017-07-13 13:00:59 +0600

    code re formatπ

--- src/controller/AppUserController.py
+++ src/controller/AppUserController.py
@@ -1,34 +1,41 @@
 from flask import Blueprint, request, jsonify
-from service.AppUserService import *
+from service.AppUserService import AppUserService
 from schema.AppUser import AppUser
 
 app_user_api = Blueprint("app_user_api", __name__)
+aus = AppUserService()
 
 
 @app_user_api.route("/")
-def appUserList():
-    return jsonify(getAppUserList())
+def getAll():
+    return jsonify(aus.getAll())
 
 
 @app_user_api.route("/<firstName>")
-def getAppUser(firstName):
-    return jsonify(getOneAppUserByName(firstName))
+def get(firstName):
+    return jsonify(aus.getOneAppUserByName(firstName))
 
 
 @app_user_api.route("/", methods=["POST"])
-def createAppUser():
+def create():
     app_user_json = request.json
     data, errors = AppUser().load(app_user_json)
     if bool(errors):
         return jsonify(errors)
-    return jsonify(createNewAppUser(data)), 201
+    return jsonify(aus.createNewAppUser(data)), 201
 
 
 @app_user_api.route("/<app_user_id>", methods=["PATCH"])
-def updateUser(app_user_id):
+def update(app_user_id):
     app_user_json = request.json
     data, errors = AppUser().load(app_user_json)
     if bool(errors):
         return jsonify(errors)
-    result = updateAppUser(app_user_id, data)
+    result = aus.updateAppUser(app_user_id, data)
+    return jsonify(result)
+
+
+@app_user_api.route("/<app_user_id>", methods=["DELETE"])
+def delete(app_user_id):
+    result = aus.delete(app_user_id)
     return jsonify(result)
--- src/service/AppUserService.py
+++ src/service/AppUserService.py
@@ -3,32 +3,40 @@ from schema.AppUser import AppUser
 from bson.objectid import ObjectId
 
 
-def getAppUserList():
-    output = []
-    for q in mongoDB.db.AppUser.find():
+class AppUserService:
+    @staticmethod
+    def getAll():
+        output = []
+        for q in mongoDB.db.AppUser.find():
+            data, errors = AppUser().load(q)
+            data['_id'] = str(q['_id'])
+            output.append(data)
+        return output
+
+    @staticmethod
+    def getOneAppUserByName(name):
+        q = mongoDB.db.AppUser.find_one_or_404({'firstName': name})
         data, errors = AppUser().load(q)
         data['_id'] = str(q['_id'])
-        output.append(data)
-    return output
+        return data
 
+    @staticmethod
+    def createNewAppUser(appUser):
+        app_user_id = mongoDB.db.AppUser.insert(appUser)
+        q = mongoDB.db.AppUser.find_one_or_404({'_id': app_user_id})
+        data, errors = AppUser().load(q)
+        return data
 
-def getOneAppUserByName(name):
-    q = mongoDB.db.AppUser.find_one_or_404({'firstName': name})
-    data, errors = AppUser().load(q)
-    data['_id'] = str(q['_id'])
-    return data
+    @staticmethod
+    def updateAppUser(appUserId, updatedAppUser):
+        mongoDB.db.AppUser.find_one_or_404({'_id': ObjectId(appUserId)})
+        mongoDB.db.AppUser.update_one({'_id': ObjectId(appUserId)}, {"$set": updatedAppUser}, upsert=False)
+        q_res = mongoDB.db.AppUser.find_one_or_404({'_id': ObjectId(appUserId)})
+        data, errors = AppUser().load(q_res)
+        return data
 
-
-def createNewAppUser(appUser):
-    app_user_id = mongoDB.db.AppUser.insert(appUser)
-    q = mongoDB.db.AppUser.find_one_or_404({'_id': app_user_id})
-    data, errors = AppUser().load(q)
-    return data
-
-
-def updateAppUser(appUserId, updatedAppUser):
-    mongoDB.db.AppUser.find_one_or_404({'_id': ObjectId(appUserId)})
-    mongoDB.db.AppUser.update_one({'_id': ObjectId(appUserId)}, {"$set": updatedAppUser}, upsert=False)
-    q_res = mongoDB.db.AppUser.find_one_or_404({'_id': ObjectId(appUserId)})
-    data, errors = AppUser().load(q_res)
-    return data
+        # @staticmethod
+        # def delete(appUserId):
+        #     mongoDB.db.AppUser.find_one_or_404({'_id': ObjectId(appUserId)})
+        #     mongoDB.db.AppUser.delete_one({'_id': ObjectId(appUserId)})
+        #     return {"delete": "success"}
