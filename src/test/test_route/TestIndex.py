56775bd83b80fd865b220b3e0925aaf24d468a8e
Md Sazzad Islam <sazzad.islam@dsinnovators.com>
2017-07-20 11:46:22 +0600

    rename refactor

--- src/test/test_route/TestIndex.py
+++ src/test/test_route/TestIndex.py
@@ -3,7 +3,7 @@ from main import app
 from flask import json
 
 
-class TestConfig(unittest.TestCase):
+class TestIndex(unittest.TestCase):
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='application/json')
